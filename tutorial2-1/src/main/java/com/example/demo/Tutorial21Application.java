3a487fc196e8aabcf0c8898dca2c7a8563b75601
jihanadibah <jhnadibah@gmail.com>
2017-09-16 20:33:29 +0700

    tutorial2

---
+++ tutorial2-1/src/main/java/com/example/demo/Tutorial21Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tutorial21Application.class, args);
+	}
+}
